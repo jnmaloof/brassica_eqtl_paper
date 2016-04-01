b7eba954996b55ffc97ae002538d255e9f22e1da
Cody Markelz <rjmarkelz@ucdavis.edu>
2016-04-01 00:14:50 -0700

    making plots

--- scripts/parental_field_analysis.R
+++ scripts/parental_field_analysis.R
@@ -1,171 +1,377 @@
 ###########
 # Cody Markelz
 # markelz@gmail.com
-# B rapa parental RNA-seq field
-# Modified March 28, 2016
+# Modified March 17, 2016
 ###########
 
-# GO enrichment?
-# metabolic differences?
+library(dplyr)
+library(data.table)
+library(ggplot2)
 
-# load libs
-library(edgeR)
-library(limma)
+# load dataset
+load('~/git.repos/brassica_eqtl_paper/input/scanone-eqtl.RData')
+ls()
 
-# data directory
-setwd("/Users/Cody_2/git.repos/brassica_parents/data")
+# choose between cis-peaks
+# trans peaks go to scaffolds
+# this is why I need the scaffold data from Mike
+dim(cistrans_df)
+head(cistrans_df)
+tail(cistrans_df)
 
-GH_counts <- read.delim("GH_merged_v1.5_mapping.tsv", row.names = NULL)
-head(GH_counts)
-rownames(GH_counts) <- GH_counts$gene
-GH_counts <- GH_counts[,-1]
+scaffolds <- cistrans_df[grepl("^Sc", cistrans_df$tx_chrom),]
+dim(scaffolds)
+head(scaffolds)
 
-#replace all NA values with 0 
-GH_counts[is.na(GH_counts)] <- 0
-head(GH_counts)
-tail(GH_counts)
-head(GH_counts)
+cis_df <- subset(cistrans_df, cis_trans == "cis")
+dim(cis_df)
+head(cis_df)
+length(unique(cis_df$tx_name))
+str(cis_df)
 
-# remove first row
-GH_counts <- GH_counts[-1,]
-head(GH_counts)[,1:10]
+cis_ag <- aggregate(lod ~ tx_name, data = cis_df, max)
+dim(cis_ag)
 
-# make field dataset
-colnames(GH_counts)
-FIELD_counts <- GH_counts[,c(13:18,43:48)]
-head(FIELD_counts)
+cis_df <- merge(cis_ag, cis_df, by = c("tx_name", "lod"))
+dim(cis_df)
+head(cis_df)
+length(unique(cis_df$tx_name))
+cis_df <- cis_df[!duplicated(cis_df$tx_name),]
 
-###########
-# clean up the colnames
-###########
-colnames(FIELD_counts) <- sub("(\\w+)(_)(\\w+)(_)(\\d)(_)(\\w+)(.+)",
-                             "\\1\\2\\3\\4\\5\\6\\7", colnames(FIELD_counts))
+dim(cis_df)
+head(cis_df)
+tail(cis_df)
+cis_df$tx_chrom
+str(cis_df)
+cis_df$qtl_pos <- as.numeric(cis_df$qtl_pos)
 
-FIELD_samples <- names(FIELD_counts)
-FIELD_samples
+cis_plot <- ggplot(cis_df)
+cis_plot <- cis_plot +  theme_bw() + geom_point(aes(x = tx_start, y = lod), size = 1.5) +
+                        facet_grid(qtl_chrom ~ . ) +
+                        theme(text = element_text(size = 20))
+cis_plot
+setwd('~/git.repos/brassica_eqtl_paper/output/')
+ggsave("cis_eqtl_plot.png", width = 10, height = 15)
 
-# tissue specific data sets
-FIELD_leaf <- FIELD_counts[,c(4:6,10:12)]
-FIELD_fruit <- FIELD_counts[,-c(4:6,10:12)]
+#get large cis effect genes
+# arbitrary cutoff
+large_cis <- cis_df[cis_df$lod > 100,]
+dim(large_cis)
+large_cis
+#73
+setwd('~/git.repos/brassica_eqtl_paper/input/')
+write.table(large_cis, "large_effect_cis.csv", sep = ",", col.names = TRUE, row.names = TRUE)
 
-head(FIELD_leaf)
 
-# field design
-FIELD_geno <- factor(sub("(\\w+)(_)(\\w+)(_)(\\d)(_)(\\w+)",
-                       "\\1", colnames(FIELD_counts)))
-FIELD_geno
+# trans eQTL
+trans_df <- subset(cistrans_df, cis_trans == "trans")
+dim(trans_df)
+trans_df <- trans_df[!grepl("^Sc", trans_df$tx_chrom),]
+dim(trans_df)
+# [1] 11024    13
 
-FIELD_tissue <- factor(sub("(\\w+)(_)(\\w+)(_)(\\d)(_)(\\w+)",
-                       "\\3", colnames(FIELD_counts)))
-FIELD_tissue
+head(trans_df)
+tail(trans_df)
 
-FIELD_geno <- relevel(FIELD_geno, ref = "R500")
-FIELD_tissue  <- relevel(FIELD_tissue, ref = "LEAF")
-
-full_model <- model.matrix(~ FIELD_geno + FIELD_tissue + FIELD_geno:FIELD_tissue)
-full_model
-
-# tissue models
-leaf_geno <- factor(sub("(\\w+)(_)(\\w+)(_)(\\d)(_)(\\w+)",
-                       "\\1", colnames(FIELD_leaf)))
-leaf_geno
-leaf_geno <- relevel(leaf_geno, ref = "R500")
-
-leaf_model <- model.matrix(~ leaf_geno)
-leaf_model
-
-fruit_geno <- factor(sub("(\\w+)(_)(\\w+)(_)(\\d)(_)(\\w+)",
-                       "\\1", colnames(FIELD_fruit)))
-fruit_geno
-
-fruit_model <- model.matrix(~ fruit_geno)
-fruit_model
-
-fruit_geno <- relevel(fruit_geno, ref = "R500")
-
-# make the necessary objects
-head(FIELD_counts)
-FIELD_DE <- DGEList(counts = FIELD_counts)
-dim(FIELD_DE)
-colnames(FIELD_DE)
-cpm(FIELD_DE)
-
-FIELD_DE <- FIELD_DE[rowSums(cpm(FIELD_DE) > 1 ) >= 5,]
-dim(FIELD_DE)
-# [1] 25740    12
-
-FIELD_DE <- calcNormFactors(FIELD_DE)
-system.time(FIELD_voom <- voom(FIELD_DE, full_model, plot = TRUE))
-system.time(FIELD_fit_full <-lmFit(FIELD_voom, full_model))
-FIELD_fit_full <- eBayes(FIELD_fit_full)
-
-?topTable
-geno_eff <- toptable(FIELD_fit_full, coef = "FIELD_genoIMB211", p.value = 0.05, n = Inf)
-dim(geno_eff)
-
-tissue_eff <- toptable(FIELD_fit_full, coef = "FIELD_tissueFRUIT", p.value = 0.05, n = Inf)
-dim(tissue_eff)
-
-str(FIELD_fit_full)
-head(FIELD_fit_full$coef)
+large_trans <- trans_df[trans_df$lod > 100,]
+dim(large_trans)
+large_trans
+#17
+setwd('~/git.repos/brassica_eqtl_paper/input/')
+write.table(large_trans, "large_effect_trans.csv", sep = ",", col.names = TRUE, row.names = TRUE)
 
 #########
-# tissue specific models
+# TODO
+# need to make sure that there are not 2 eQTL per chromosome
+# 1 MB cutoff?
 #########
 
-# leaf models
-head(FIELD_leaf)
-leaf_de <- DGEList(counts = FIELD_leaf)
-dim(leaf_de)
-colnames(leaf_de)
-leaf_de <- leaf_de[rowSums(cpm(leaf_de) > 1 ) >= 5,]
-dim(leaf_de)
-# [1] 20725     6
+# we really do not have the resolution to care about how EXACT the qtl is next
+# to the start site for the cis so we should just choose largest lod
+# that is the statistical information that we have for the population
 
-leaf_de <- calcNormFactors(leaf_de)
-system.time(leaf_voom <- voom(leaf_de, leaf_model, plot = TRUE))
-system.time(leaf_fit <- lmFit(leaf_voom, leaf_model))
-leaf_fit <- eBayes(leaf_fit)
-leaf_fit
-leaf_genes <- topTable(leaf_fit, p.value = 0.05, n = Inf)
-dim(leaf_genes)
-# [1] 4435    6
-head(leaf_genes)
-tail(leaf_genes)
+trans_ag <- aggregate(lod ~ tx_name, data = trans_df, max)
+dim(trans_ag)
+head(trans_ag)
+trans_df <- merge(trans_ag, trans_df, by = c("tx_name", "lod"))
+dim(trans_df)
+head(trans_df)
+trans_df$qtl_pos <- as.numeric(trans_df$qtl_pos)
 
-# fruit models
-head(FIELD_fruit)
-fruit_de <- DGEList(counts = FIELD_fruit)
-dim(fruit_de)
-colnames(fruit_de)
-fruit_de <- fruit_de[rowSums(cpm(fruit_de) > 1 ) >= 5,]
-dim(fruit_de)
-# [1] 22511     6
+trans_plot <- ggplot(trans_df)
+trans_plot <- trans_plot +  theme_bw() + geom_point(aes(x = qtl_pos, y = lod), size = 1.5) +
+                        facet_grid(qtl_chrom ~ . )
+                        theme(text = element_text(size = 20))
+trans_plot
+setwd('~/git.repos/brassica_eqtl_paper/output/')
+ggsave("trans_eqtl_plot.png", width = 10, height = 10)
 
-fruit_de <- calcNormFactors(fruit_de)
-system.time(fruit_voom <- voom(fruit_de, fruit_model, plot = TRUE))
-system.time(fruit_fit <-lmFit(fruit_voom, fruit_model))
-fruit_fit <- eBayes(fruit_fit)
-fruit_fit
-fruit_genes <- topTable(fruit_fit, p.value = 0.05, n = Inf)
-dim(fruit_genes)
-# [1] 2967    6
-head(fruit_genes)
+head(trans_df)
+head(cis_df)
+str(cis_df)
+str(trans_df)
 
-# overlap genes
-merged_genes <- merge(leaf_genes, fruit_genes, by = "row.names")
-head(merged_genes)
-dim(merged_genes)
+# put data together
+ct_merge <- rbind(cis_df, trans_df)
+head(ct_merge)
+dim(ct_merge)
+str(ct_merge)
 
-# write tables
-setwd("/Users/Cody_2/git.repos/brassica_parents/data")
-?write.table
-write.table(leaf_genes, "parental_leaf_field_DE.csv", sep = ",", row.names = TRUE, col.names = TRUE)
-write.table(fruit_genes, "parental_fruit_field_DE.csv", sep = ",", row.names = TRUE, col.names = TRUE)
-write.table(merged_genes, "parental_merged_field_DE.csv", sep = ",", row.names = TRUE, col.names = TRUE)
+merge_plot <- ggplot(ct_merge)
+merge_plot <- merge_plot +  theme_bw() + geom_point(aes(x = qtl_pos, y = lod, color = cis_trans), size = 2) +
+                        facet_grid(qtl_chrom ~ . )
+                        theme(text = element_text(size = 20))
+merge_plot
+setwd('~/git.repos/brassica_eqtl_paper/output/')
+ggsave("cis_trans_eqtl_plot.png", width = 10, height = 10)
 
-# go enrichment
+# cis trans plot
+merge_plot <- ggplot(ct_merge)
+merge_plot <- merge_plot + geom_point(aes(x = qtl_pos, y = tx_start, color = lod ), size = 1.5) +
+                        scale_y_reverse() +
+                        facet_grid(tx_chrom ~ qtl_chrom) + theme_bw() + 
+                        theme(axis.ticks = element_blank(), axis.text.x = element_blank(),
+                         axis.text.y = element_blank())
+merge_plot
+?ggsave
+ggsave("cis_diagonal.png", width = 10, height = 10, dpi = 300)
+
+############
+# flowering gene distribution
+############
+
+setwd('~/git.repos/brassica_eqtl_v1.5/data/')
+br_flr <- read.delim("br_flowering_genes.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+head(br_flr)
+dim(br_flr)
+colnames(br_flr)
+
+str(flr_genes)
+flr_genes <- br_flr[9]
+colnames(flr_genes)[1] <- "gene_name"
+head(flr_genes)
+
+trans_df$tx_name <- as.character(trans_df$tx_name)
+cis_df$tx_name <- as.character(cis_df$tx_name)
+
+trans_flr_df <- trans_df[trans_df$tx_name %in% flr_genes$gene_name,]
+head(trans_flr_df)
+
+trans_flr_plot <- ggplot(trans_df)
+trans_flr_plot <- trans_flr_plot + theme_bw() + geom_point(aes(x = qtl_pos, y = lod), size = 1.5) +
+                facet_grid(qtl_chrom ~ . ) + 
+                geom_segment(data = trans_flr_df, aes(x = qtl_pos, xend = qtl_pos), y = 0 , yend = 100, color = 'red') +
+                theme(text = element_text(size = 20))
+trans_flr_plot
+setwd('~/git.repos/brassica_eqtl_paper/output/')
+ggsave("trans_flr_plot.png", width = 10, height = 10)
+
+cis_flr_df <- cis_df[cis_df$tx_name %in% flr_genes$gene_name,]
+head(cis_flr_df)
+head(trans_df)
+dim(cis_flr_df)
+
+cis_flr_plot <- ggplot(cis_df)
+cis_flr_plot <- cis_flr_plot + theme_bw() + geom_point(aes(x = qtl_pos, y = lod), size = 1.5) +
+                facet_grid(qtl_chrom ~ . ) + 
+                geom_segment(data = cis_flr_df, aes(x = qtl_pos, xend = qtl_pos), y = 0 , yend = 100, color = 'red') +
+                theme(text = element_text(size = 20))
+cis_flr_plot
+setwd('~/git.repos/brassica_eqtl_paper/output/')
+ggsave("cis_flr_plot.png", width = 10, height = 10)
+
+############
+# parental analysis data
+############
+setwd('~/git.repos/brassica_parents/data/')
+
+br_fruits <- read.delim("parental_fruit_field_DE.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+head(br_fruits)
+tail(br_fruits)
+dim(br_fruits)
+br_fruits$tx_name <- rownames(br_fruits)
+
+cis_de_df <- cis_df[cis_df$tx_name %in% br_fruits$tx_name,]
+head(cis_de_df)
+dim(cis_de_df)
+dim(cis_df)
+
+trans_de_df <- trans_df[trans_df$tx_name %in% br_fruits$tx_name,]
+head(trans_de_df)
+dim(trans_de_df)
+dim(trans_df)
+
+cis_de_plot <- ggplot(cis_df)
+cis_de_plot <- cis_de_plot + theme_bw() + geom_point(aes(x = qtl_pos, y = lod), size = 1.5) +
+                facet_grid(qtl_chrom ~ . ) + 
+                geom_segment(data = cis_de_df, aes(x = qtl_pos, xend = qtl_pos), y = 0 , yend = 100, color = 'red') +
+                theme(text = element_text(size = 20))
+cis_de_plot
+
+ggsave("cis_de_fruit_plot.png", width = 10, height = 10)
+
+trans_de_plot <- ggplot(trans_df)
+trans_de_plot <- trans_de_plot + theme_bw() + geom_point(aes(x = qtl_pos, y = lod), size = 1.5) +
+                facet_grid(qtl_chrom ~ . ) + 
+                geom_segment(data = trans_de_df, aes(x = qtl_pos, xend = qtl_pos), y = 0 , yend = 100, color = 'red') +
+                theme(text = element_text(size = 20))
+trans_de_plot
+ggsave("trans_de_fruit_plot.png", width = 10, height = 10)
 
 
+br_leaf <- read.delim("parental_leaf_field_DE.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+head(br_leaf)
+dim(br_leaf)
+br_leaf$tx_name <- rownames(br_leaf)
 
-#end
+
+cis_de_leaf_df <- cis_df[cis_df$tx_name %in% br_leaf$tx_name,]
+head(cis_de_leaf_df)
+dim(cis_de_leaf_df)
+dim(cis_df)
+
+trans_de_leaf_df <- trans_df[trans_df$tx_name %in% br_leaf$tx_name,]
+head(trans_de_leaf_df)
+dim(trans_de_leaf_df)
+dim(trans_df)
+
+cis_de_leaf_plot <- ggplot(cis_df)
+cis_de_leaf_plot <- cis_de_leaf_plot + theme_bw() + geom_point(aes(x = qtl_pos, y = lod), size = 1.5) +
+                facet_grid(qtl_chrom ~ . ) + 
+                geom_segment(data = cis_de_leaf_df, aes(x = qtl_pos, xend = qtl_pos), y = 0 , yend = 100, color = 'red') +
+                theme(text = element_text(size = 20))
+cis_de_leaf_plot
+ggsave("cis_de_leaf_plot.png", width = 10, height = 10)
+
+trans_de_leaf_plot <- ggplot(trans_df)
+trans_de_leaf_plot <- trans_de_leaf_plot + theme_bw() + geom_point(aes(x = qtl_pos, y = lod), size = 1.5) +
+                facet_grid(qtl_chrom ~ . ) + 
+                geom_segment(data = trans_de_leaf_df, aes(x = qtl_pos, xend = qtl_pos), y = 0 , yend = 100, color = 'red') +
+                theme(text = element_text(size = 20))
+trans_de_leaf_plot
+ggsave("trans_de_leaf_plot.png", width = 10, height = 10)
+
+#############
+# eQTL GO enrichment
+#############
+library(Biostrings)
+library(GO.db)
+library(goseq)
+
+# infile and manipulate go annotation file
+setwd("/Users/Cody_2/git.repos/brassica_genome_db/raw_data")
+brass_go <- read.table("Brassica_rapa_v1.5_final_annot.wego", header = FALSE)
+head(brass_go)
+colnames(brass_go) <- c("Gene", "GO")
+dim(brass_go)
+
+# make list object to store data
+brass_go_list <- strsplit(as.character(brass_go[,2]), split=",", fixed=T)
+head(brass_go_list)
+names(brass_go_list) <- as.character(brass_go[,1])
+head(brass_go_list)
+length(brass_go_list)
+
+# also need to infile the gene length data as goseq uses this to estimate any biases based on gene
+# lengths in the dataset. 
+brass_gene_lengths <- read.table("Brassica_rapa_v1.5_final_gene_lengths", header = FALSE)
+head(brass_gene_lengths)
+colnames(brass_gene_lengths) <- c("Gene", "length")
+dim(brass_gene_lengths)
+str(brass_gene_lengths)
+# [1] 43463     2
+brass_gene_lengths$Gene <- as.character(brass_gene_lengths$Gene)
+dim(brass_gene_lengths)
+# [1] 43463     2
+
+gene_names <- as.data.frame(high1$names)
+head(gene_names)
+names(gene_names) <- "tx_name"
+keeps <- as.character(gene_names$tx_name)
+# subset brass_gene_lengths for eQTL genes
+# cis df
+brass_gene_lengths_red <- brass_gene_lengths[brass_gene_lengths$Gene %in% keeps,]
+str(brass_gene_lengths_red)
+# 35039 total genes tested for eQTL
+
+# of total genes tested need to get the ones that are significant for whatever
+keeps_cis <- as.character(cis_df$tx_name)
+head(keeps_cis)
+cis_gene_lengths <- brass_gene_lengths_red[brass_gene_lengths_red$Gene %in% keeps_cis,]
+not_cis_gene <- brass_gene_lengths_red[!brass_gene_lengths_red$Gene %in% keeps_cis,]
+
+dim(cis_gene_lengths)
+head(cis_gene_lengths)
+# [1] 8907    2
+dim(not_cis_gene)
+head(not_cis_gene)
+# [1] 26132     2
+
+nullp_vector <- rep(c(1,0),c(nrow(cis_gene_lengths), nrow(not_cis_gene)))
+head(nullp_vector)
+tail(nullp_vector)
+names(nullp_vector) <- c(cis_gene_lengths$Gene, not_cis_gene$Gene)
+
+bias_data_vector <- c(cis_gene_lengths$length, not_cis_gene$length)
+head(bias_data_vector)
+# ?rep
+# ?nullp
+
+brass_nullp <- nullp(nullp_vector, genome = NULL, id = NULL, bias.data = bias_data_vector)
+# # Warning message:
+# # In pcls(G) : initial point very close to some inequality constraints
+
+# ?goseq
+# # need to decide whether or not to use the useuse_genes_without_cat=TRUE option. 
+# # ~8000 genes not included in analysis that do not have a GO category classification
+go_analysis_cis  <-  goseq(brass_nullp, gene2cat = brass_go_list, use_genes_without_cat = TRUE)
+head(go_analysis_cis, 100)
+
+setwd('~/git.repos/brassica_eqtl_v1.5/output/')
+write.table(go_analysis_cis, "cis_eqtl_enrichment.csv", sep = ",", col.names = TRUE, row.names = TRUE)
+
+##########
+#Trans
+##########
+
+# of total genes tested need to get the ones that are significant for whatever
+keeps_trans <- as.character(trans_df$tx_name)
+head(keeps_trans)
+length(keeps_trans)
+# [1] 3749
+trans_gene_lengths <- brass_gene_lengths_red[brass_gene_lengths_red$Gene %in% keeps_trans,]
+not_trans_gene <- brass_gene_lengths_red[!brass_gene_lengths_red$Gene %in% keeps_trans,]
+
+dim(trans_gene_lengths)
+head(trans_gene_lengths)
+# [1] 3596    2
+
+dim(not_trans_gene)
+head(not_trans_gene)
+# [1] 31443     2
+
+nullp_vector <- rep(c(1,0),c(nrow(trans_gene_lengths), nrow(not_trans_gene)))
+head(nullp_vector)
+tail(nullp_vector)
+names(nullp_vector) <- c(trans_gene_lengths$Gene, not_trans_gene$Gene)
+
+bias_data_vector <- c(trans_gene_lengths$length, not_trans_gene$length)
+head(bias_data_vector)
+
+brass_nullp <- nullp(nullp_vector, genome = NULL, id = NULL, bias.data = bias_data_vector)
+# # Warning message:
+# # In pcls(G) : initial point very close to some inequality constraints
+
+# ?goseq
+# # need to decide whether or not to use the useuse_genes_without_cat=TRUE option. 
+# # ~8000 genes not included in analysis that do not have a GO category classification
+go_analysis_trans  <-  goseq(brass_nullp, gene2cat = brass_go_list, use_genes_without_cat = TRUE)
+head(go_analysis_trans, 100)
+
+setwd('~/git.repos/brassica_eqtl_v1.5/output/')
+write.table(go_analysis_trans, "trans_eqtl_enrichment.csv", sep = ",", col.names = TRUE, row.names = TRUE)
+
+setwd("/Users/Cody_2/git.repos/brassica_eqtl_v1.5/data")
+save.image(file = "un_eqtl_parent_field.RData", version = NULL, ascii = FALSE, safe = TRUE)
+
+# end
